426efd811cf6c700bf9c514e6e73065db9496b06
aryark <aryark3112@gmail.com>
2023-07-19 11:23:01 +0530

    first commit

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace _19JulyConstructorEx
+{
+
+    public class Employee
+    {
+        int id;
+        string name;
+        public Employee()
+        {
+            Console.WriteLine("Constructor of Employee");
+            id = -1;
+            name = "not given";
+        }
+        public void Register()
+        {
+            Console.WriteLine("Enter id");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter name");
+            name = Console.ReadLine();
+        }
+        public void Display()
+
+        {
+            Console.WriteLine("ID:\t" + id);
+            Console.WriteLine("Name:\t" + name);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19JulyConstructorEx
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee employee = new Employee();
+            employee.Display();
+            Console.WriteLine("Let us go for Registration!!!");
+            employee.Register();
+            employee.Display();
+            Console.ReadKey();
+        }
+    }
+}
